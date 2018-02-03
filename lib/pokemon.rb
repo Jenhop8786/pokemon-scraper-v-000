445c0a623653bb5ed4d5cdab1a0267ef1285cb64
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-03 02:54:23 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,10 +1,15 @@
 class Pokemon
   attr_accessor :id, :name, :type, :hp, :db
 
-  def initialize (id:, name:, type:, hp:nil, db:)
+def initialize (id:, name:, type:, hp:nil, db:)
     @id = id
     @name = name
     @type = type
     @hp = hp
     @db = db  
 end
+
+def self.save (name, type, db)
+  pokemon = db.execute ("SELECT * FROM pokemon WHERE id=?", id).flatten
+  Pokemon.new (id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
+end 
